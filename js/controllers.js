9140c8a41a3c524061f36e6beead12db986d31df
John Duong <jhnduong@gmail.com>
2016-04-08 14:41:25 -0700

    added console.logging the audio time

--- js/controllers.js
+++ js/controllers.js
@@ -2,4 +2,7 @@ var appController = angular.module('appController', ['ngAudio']);
 
 appController.controller('audioCtrl',function($scope,ngAudio){
     $scope.audio = ngAudio.load("media/horse.mp3"); // returns NgAudioObject
+    $scope.hello = function(){
+    	console.log($scope.audio.currentTime);
+    };
 })
