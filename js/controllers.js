06f96f7f3307f914a4c8aa0071438617455e7413
John Duong <jhnduong@gmail.com>
2016-04-09 12:11:19 -0700

    Add backend dependencies and package.json

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,13 @@
 var appController = angular.module('appController', ['ngAudio']);
 
-appController.controller('audioCtrl',function($scope,ngAudio){
+appController.controller('audioCtrl',function($scope, $window, ngAudio){
     $scope.audio = ngAudio.load("media/horse.mp3"); // returns NgAudioObject
     $scope.hello = function(){
     	console.log($scope.audio.currentTime);
     };
+    $scope.onExit = function() {
+    	//something
+    };
+    $window.onbeforeunload =  $scope.onExit;
+
 })
